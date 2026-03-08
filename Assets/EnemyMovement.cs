e7dfec489a688719542ec09c4548522a2b454f7d
4lmaLittera <4lmalittera@gmail.com>
2026-03-09 00:54:47 +0200

    fear: add walking biguys animation, enemy spawner implementation

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Serialization;
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyMovement : MonoBehaviour, IPoolableEnemy
 {
     #region Serialized Fields
     [Header("References")]
@@ -15,9 +15,19 @@ public class EnemyMovement : MonoBehaviour
     #endregion
 
     #region Unity Lifecycle
-    void Start()
+    void Awake()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        // Try to find player if not assigned
+        if (_player == null)
+        {
+            var playerObj = GameObject.FindGameObjectWithTag("Player");
+            if (playerObj != null) _player = playerObj.transform;
+        }
     }
 
     void Update()
@@ -25,6 +35,31 @@ public class EnemyMovement : MonoBehaviour
         if (_player != null && _navMeshAgent != null && _navMeshAgent.isActiveAndEnabled && _navMeshAgent.isOnNavMesh)
         {
             _navMeshAgent.SetDestination(_player.position);
+        }
+    }
+    #endregion
+
+    #region IPoolableEnemy Implementation
+    public void OnSpawn()
+    {
+        if (_navMeshAgent != null)
+        {
+            // Warp is essential to prevent the agent from trying to walk from its dead body location to the new spawn point
+            _navMeshAgent.Warp(transform.position);
+            
+            // Only reset path if the agent is fully initialized and active on the mesh
+            if (_navMeshAgent.isActiveAndEnabled && _navMeshAgent.isOnNavMesh)
+            {
+                _navMeshAgent.ResetPath();
+            }
+        }
+    }
+
+    public void OnDespawn()
+    {
+        if (_navMeshAgent != null && _navMeshAgent.isActiveAndEnabled && _navMeshAgent.isOnNavMesh)
+        {
+            _navMeshAgent.ResetPath();
         }
     }
     #endregion
